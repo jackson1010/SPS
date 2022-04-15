c36e2d6ec94f22edeb0686a08bd9cca053d143ee
jackson1010 <43661926+jackson1010@users.noreply.github.com>
2022-04-15 17:09:30 +0800

    beat that

--- day05-mad-libs/in-class/script.js
+++ day05-mad-libs/in-class/script.js
@@ -1,35 +1,96 @@
+var getRandomIndex = function (arrayLength) {
+  var randomIndex = Math.floor(Math.random() * arrayLength);
+  return randomIndex;
+};
+
+var words = [];
 var madLibsAdjectivesMain = function (input) {
-  // Complete the Base: Mad Libs Adjectives exercise below with madLibsAdjectivesMain as the main function.
-  var myOutputValue = 'hello world';
-  return myOutputValue;
+  if (input == "create") {
+    var index = getRandomIndex(words.length);
+    return `"WOW!" he said EXCITEDLY as he jumped into his convertible PAPAYA and drove off with his ${words[index]} wife.`;
+  }
+  words.push(input);
+  return `You entered ${words}, enter an adjective or type "create" to madlib.`;
 };
 
+var mode = "input";
+var words2 = [];
 var madLibsInputCreateMain = function (input) {
-  // Complete the Comfortable: Input and Create Mode exercise below with madLibsInputCreateMain as the main function.
-  var myOutputValue = 'hello world';
-  return myOutputValue;
+  if (input == "input") {
+    mode = "input";
+    return `
+    You have switched to input mode.<br/><br/>
+    Enter an adjective or type "create" to switch to create mode.`;
+  }
+
+  if (input == "create") {
+    mode = "create";
+    return `
+    You have switched to create mode.<br/><br/>
+    Hit Submit to madlib.`;
+  }
+
+  if (mode == "create") {
+    var index = getRandomIndex(words2.length);
+    return `"WOW!" he said EXCITEDLY as he jumped into his convertible PAPAYA and drove off with his ${words2[index]} wife.<br> 
+    Hit Submit again to madlib or type "input" to switch to input mode.`;
+  }
+
+  if (mode == "input" && input.trim() != "") {
+    words2.push(input);
+    return `You have added ${words2}, enter another adjective or type "create" to madlib. `;
+  }
 };
 
+var words3 = [];
 var madLibsMultipleWordsMain = function (input) {
   // Complete the Comfortable: Input Multiple Words exercise below with madLibsMultipleWordsMain as the main function.
-  var myOutputValue = 'hello world';
-  return myOutputValue;
+  if (input == "input") {
+    mode = "input";
+    return `
+    You have switched to input mode.<br/><br/>
+    Enter an adjective or type "create" to switch to create mode.`;
+  }
+
+  if (input == "create") {
+    mode = "create";
+    return `
+    You have switched to create mode.<br/><br/>
+    Hit Submit to madlib.`;
+  }
+
+  if (mode == "create") {
+    var index = getRandomIndex(words3.length);
+    return `"WOW!" he said EXCITEDLY as he jumped into his convertible PAPAYA and drove off with his ${words3[index]} wife.<br> 
+    Hit Submit again to madlib or type "input" to switch to input mode.`;
+  }
+
+  if (mode == "input") {
+    if (input.trim() == "") {
+      return `Please input adjectives separated by a space each to fill in our Mad Lib.`;
+    }
+    var manywords = input.split(" ");
+    //for (var counter = 0; counter < manywords.length; counter++) {
+    // words3.push(manywords[counter])};
+    words3 = words3.concat(manywords);
+    return `You have added ${words3}, enter another adjective or type "create" to madlib. `;
+  }
 };
 
 var madLibsMultipleTypesMain = function (input) {
   // Complete the More Comfortable: Mad Libs Multiple Word Types exercise below with madLibsMultipleTypesMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
 
 var madLibsPopularMain = function (input) {
   // Complete the More Comfortable: Popular Mad Libs exercise below with madLibsPopularMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
 
 var madLibsSetsMain = function (input) {
   // Complete the More Comfortable: Sets of Mad Libs exercise below with madLibsSetsMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
--- day07-moar-cards/pre-class/script.js
+++ day07-moar-cards/pre-class/script.js
@@ -1,17 +1,479 @@
+var deck = [
+  {
+    name: "ace",
+    suit: "hearts",
+    rank: 1
+  },
+  {
+    name: "2",
+    suit: "hearts",
+    rank: 2
+  },
+  {
+    name: "3",
+    suit: "hearts",
+    rank: 3
+  },
+  {
+    name: "4",
+    suit: "hearts",
+    rank: 4
+  },
+  {
+    name: "5",
+    suit: "hearts",
+    rank: 5
+  },
+  {
+    name: "6",
+    suit: "hearts",
+    rank: 6
+  },
+  {
+    name: "7",
+    suit: "hearts",
+    rank: 7
+  },
+  {
+    name: "8",
+    suit: "hearts",
+    rank: 8
+  },
+  {
+    name: "9",
+    suit: "hearts",
+    rank: 9
+  },
+  {
+    name: "10",
+    suit: "hearts",
+    rank: 10
+  },
+  {
+    name: "jack",
+    suit: "hearts",
+    rank: 11
+  },
+  {
+    name: "queen",
+    suit: "hearts",
+    rank: 12
+  },
+  {
+    name: "king",
+    suit: "hearts",
+    rank: 13
+  },
+  {
+    name: "ace",
+    suit: "diamonds",
+    rank: 1
+  },
+  {
+    name: "2",
+    suit: "diamonds",
+    rank: 2
+  },
+  {
+    name: "3",
+    suit: "diamonds",
+    rank: 3
+  },
+  {
+    name: "4",
+    suit: "diamonds",
+    rank: 4
+  },
+  {
+    name: "5",
+    suit: "diamonds",
+    rank: 5
+  },
+  {
+    name: "6",
+    suit: "diamonds",
+    rank: 6
+  },
+  {
+    name: "7",
+    suit: "diamonds",
+    rank: 7
+  },
+  {
+    name: "8",
+    suit: "diamonds",
+    rank: 8
+  },
+  {
+    name: "9",
+    suit: "diamonds",
+    rank: 9
+  },
+  {
+    name: "10",
+    suit: "diamonds",
+    rank: 10
+  },
+  {
+    name: "jack",
+    suit: "diamonds",
+    rank: 11
+  },
+  {
+    name: "queen",
+    suit: "diamonds",
+    rank: 12
+  },
+  {
+    name: "king",
+    suit: "diamonds",
+    rank: 13
+  },
+  {
+    name: "ace",
+    suit: "clubs",
+    rank: 1
+  },
+  {
+    name: "2",
+    suit: "clubs",
+    rank: 2
+  },
+  {
+    name: "3",
+    suit: "clubs",
+    rank: 3
+  },
+  {
+    name: "4",
+    suit: "clubs",
+    rank: 4
+  },
+  {
+    name: "5",
+    suit: "clubs",
+    rank: 5
+  },
+  {
+    name: "6",
+    suit: "clubs",
+    rank: 6
+  },
+  {
+    name: "7",
+    suit: "clubs",
+    rank: 7
+  },
+  {
+    name: "8",
+    suit: "clubs",
+    rank: 8
+  },
+  {
+    name: "9",
+    suit: "clubs",
+    rank: 9
+  },
+  {
+    name: "10",
+    suit: "clubs",
+    rank: 10
+  },
+  {
+    name: "jack",
+    suit: "clubs",
+    rank: 11
+  },
+  {
+    name: "queen",
+    suit: "clubs",
+    rank: 12
+  },
+  {
+    name: "king",
+    suit: "clubs",
+    rank: 13
+  },
+  {
+    name: "ace",
+    suit: "spades",
+    rank: 1
+  },
+  {
+    name: "2",
+    suit: "spades",
+    rank: 2
+  },
+  {
+    name: "3",
+    suit: "spades",
+    rank: 3
+  },
+  {
+    name: "4",
+    suit: "spades",
+    rank: 4
+  },
+  {
+    name: "5",
+    suit: "spades",
+    rank: 5
+  },
+  {
+    name: "6",
+    suit: "spades",
+    rank: 6
+  },
+  {
+    name: "7",
+    suit: "spades",
+    rank: 7
+  },
+  {
+    name: "8",
+    suit: "spades",
+    rank: 8
+  },
+  {
+    name: "9",
+    suit: "spades",
+    rank: 9
+  },
+  {
+    name: "10",
+    suit: "spades",
+    rank: 10
+  },
+  {
+    name: "jack",
+    suit: "spades",
+    rank: 11
+  },
+  {
+    name: "queen",
+    suit: "spades",
+    rank: 12
+  },
+  {
+    name: "king",
+    suit: "spades",
+    rank: 13
+  }
+];
+
+// Get a random index ranging from 0 (inclusive) to max (exclusive).
+var getRandomIndex = function (max) {
+  return Math.floor(Math.random() * max);
+};
+
+// Shuffle the elements in the cardDeck array
+var shuffleCards = function (cardDeck) {
+  // Loop over the card deck array once
+  var currentIndex = 0;
+  while (currentIndex < cardDeck.length) {
+    // Select a random index in the deck
+    var randomIndex = getRandomIndex(cardDeck.length);
+    // Select the card that corresponds to randomIndex
+    var randomCard = cardDeck[randomIndex];
+    // Select the card that corresponds to currentIndex
+    var currentCard = cardDeck[currentIndex];
+    // Swap positions of randomCard and currentCard in the deck
+    cardDeck[currentIndex] = randomCard;
+    cardDeck[randomIndex] = currentCard;
+    // Increment currentIndex
+    currentIndex = currentIndex + 1;
+  }
+  // Return the shuffled deck
+  return cardDeck;
+};
+
 var followObjectsMain = function (input) {
-  // Attempt the Follow Along exercise from the Objects module below with followObjectsMain as the main function.
-  var myOutputValue = 'hello world';
+  // Shuffle the deck and save it in a new variable shuffledDeck
+  // to communicate that we have shuffled the deck.
+  var shuffledDeck = shuffleCards(deck);
+
+  // Draw 2 cards from the top of the deck
+  var computerCard = shuffledDeck.pop();
+  var playerCard = shuffledDeck.pop();
+
+  // Construct an output string to communicate which cards were drawn
+  var myOutputValue =
+    "Computer had " +
+    computerCard.name +
+    " of " +
+    computerCard.suit +
+    ". Player had " +
+    playerCard.name +
+    " of " +
+    playerCard.suit +
+    ". ";
+
+  // Compare computer and player cards by rank attribute
+  // If computer card rank is greater than player card rank, computer wins
+  if (computerCard.rank > playerCard.rank) {
+    // Add conditional-dependent text to the output string
+    myOutputValue = myOutputValue + "Computer wins.";
+    // Else if computer card rank is less than player card rank, player wins
+  } else if (computerCard.rank < playerCard.rank) {
+    myOutputValue = myOutputValue + "Player wins!";
+    // Otherwise (i.e. ranks are equal), it's a tie
+  } else {
+    myOutputValue = myOutputValue + "It's a tie.";
+  }
+
+  // Return the fully-constructed output string
   return myOutputValue;
 };
 
 var highestCardsMain = function (input) {
-  // Attempt the Highest of 2 Cards exercise from the Objects module below with highestCardsMain as the main function.
-  var myOutputValue = 'hello world';
+  var shuffledDeck = shuffleCards(deck);
+
+  // Draw 2 cards from the top of the deck
+  var computerCard = shuffledDeck.pop();
+  var computerCard2 = shuffledDeck.pop();
+  var playerCard = shuffledDeck.pop();
+  var playerCard2 = shuffledDeck.pop();
+
+  var myOutputValue =
+    "Computer had " +
+    computerCard.name +
+    " of " +
+    computerCard.suit +
+    " and " +
+    computerCard2.name +
+    " of " +
+    computerCard2.suit +
+    ". Player had " +
+    playerCard.name +
+    " of " +
+    playerCard.suit +
+    " and " +
+    playerCard2.name +
+    " of " +
+    playerCard2.suit +
+    ". ";
+  var count = 0;
+  var count2 = 0;
+  if (
+    computerCard.rank > playerCard.rank &&
+    computerCard.rank > playerCard2.rank
+  ) {
+    // Add conditional-dependent text to the output string
+    count += 1;
+    // Else if computer card rank is less than player card rank, player wins
+  }
+
+  if (
+    computerCard2.rank > playerCard.rank &&
+    computerCard2.rank > playerCard2.rank
+  ) {
+    // Add conditional-dependent text to the output string
+    count += 1;
+    // Else if computer card rank is less than player card rank, player wins
+  }
+
+  if (
+    playerCard.rank > computerCard.rank &&
+    playerCard.rank > computerCard2.rank
+  ) {
+    count2 += 1;
+    // Otherwise (i.e. ranks are equal), it's a tie
+  }
+
+  if (
+    playerCard2.rank > computerCard.rank &&
+    playerCard2.rank > computerCard2.rank
+  ) {
+    count2 += 1;
+    // Otherwise (i.e. ranks are equal), it's a tie
+  }
+
+  if (count == 0 && count2 == 0) {
+    myOutputValue = myOutputValue + "Its a tie!";
+  } else if (count2 > 0) {
+    myOutputValue = myOutputValue + "Player Win!";
+  } else myOutputValue = myOutputValue + "Computer Win!";
+
   return myOutputValue;
 };
 
+var makeDeck = function () {
+  // Initialise an empty deck array
+  var cardDeck = [];
+  // Initialise an array of the 4 suits in our deck. We will loop over this array.
+  var suits = ["hearts", "diamonds", "clubs", "spades"];
+
+  // Loop over the suits array
+  for (var i; i < suits.length; i += 1) {
+    // Store the current suit in a variable
+    var currentSuit = suits[i];
+    // Loop from 1 to 13 to create all cards for a given suit
+    // Notice rankCounter starts at 1 and not 0, and ends at 13 and not 12.
+    // This is an example of a loop without an array.
+    for (var rankCounter = 1; rankCounter <= 13; rankCounter += 1) {
+      // By default, the card name is the same as rankCounter
+      var cardName = rankCounter;
+      // If rank is 1, 11, 12, or 13, set cardName to the ace or face card's name
+      if (cardName == 1) {
+        cardName = "ace";
+      } else if (cardName == 11) {
+        cardName = "jack";
+      } else if (cardName == 12) {
+        cardName = "queen";
+      } else if (cardName == 13) {
+        cardName = "king";
+      }
+      // Create a new card with the current name, suit, and rank
+      var card = {
+        name: cardName,
+        suit: currentSuit,
+        rank: rankCounter
+      };
+      // Add the new card to the deck
+      cardDeck.push(card);
+    }
+  }
+
+  // Return the completed card deck
+  return cardDeck;
+};
+
 var followCreatingMain = function (input) {
-  // Attempt the Follow Along exercise from the Creating Objects with Loops module below with followCreatingMain as the main function.
-  var myOutputValue = 'hello world';
-  return myOutputValue;
+  var cardDeck = [];
+  // Initialise an array of the 4 suits in our deck. We will loop over this array.
+  var suits = ["hearts", "diamonds", "clubs", "spades"];
+
+  // Loop over the suits array
+  for (var i; i < suits.length; i += 1) {
+    // Store the current suit in a variable
+    var currentSuit = suits[i];
+    // Loop from 1 to 13 to create all cards for a given suit
+    // Notice rankCounter starts at 1 and not 0, and ends at 13 and not 12.
+    // This is an example of a loop without an array.
+    for (var rankCounter = 1; rankCounter <= 13; rankCounter += 1) {
+      // By default, the card name is the same as rankCounter
+      var cardName = rankCounter;
+      // If rank is 1, 11, 12, or 13, set cardName to the ace or face card's name
+      if (cardName == 1) {
+        cardName = "ace";
+      } else if (cardName == 11) {
+        cardName = "jack";
+      } else if (cardName == 12) {
+        cardName = "queen";
+      } else if (cardName == 13) {
+        cardName = "king";
+      }
+      // Create a new card with the current name, suit, and rank
+      var card = {
+        name: cardName,
+        suit: currentSuit,
+        rank: rankCounter
+      };
+      // Add the new card to the deck
+      cardDeck.push(card);
+    }
+  }
+
+  // Return the completed card deck
+  return cardDeck;
 };
